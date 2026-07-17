7b8106b9475ded72f0e3cf9413655fc6d7fddcd4
Mahitha Gopisetti <mahithagopisetti0@gmail.com>
2026-07-17 13:59:44 +0530

    hello commit

---
+++ src/test/java/com/mahithag/Autosuggest/AutosuggestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahithag.Autosuggest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosuggestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
